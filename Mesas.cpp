da5b2213efd75e99341d23731494e51e3fbb7885
JDavidFr <49641134+JDavidFr@users.noreply.github.com>
2019-04-16 13:15:38 +0100

    Add files via upload

---
+++ Mesas.cpp
@@ -0,0 +1,103 @@
+#include"Mesas.h"
+#include"Pessoas.h"
+#include"Ficheiros.h"
+#include<iostream>
+#include<string>
+using namespace std;
+
+refeição* novaMeal() {//vai receber os dados do utilizador para uma nova refeição
+	cout << "***** REFEIÇÃO NOVA *****\nA cantina EDA necessita de uma nova refeição\n\t1. Introduza a entrada:" << endl;
+	refeição *meal = new refeição();
+	getline(cin, meal->entrada);
+	cout << "\t2. Introduza o prato principal:" << endl;
+	getline(cin, meal->pratoMain);
+	cout << "\t3. Introduza o preço:" << endl;
+	cin >> meal->custo;
+	return meal;
+}
+
+string* escreveMeal(refeição *r) {//escreve a refeição atual, retorna um array de strings para guardar-se para a possível gravação de dados
+	string *aux1 = new string(r->entrada);
+	string *aux2 = new string(r->pratoMain);
+	cout << "Refeição atual:\n\tEntrada: " << *aux1 << "\n\tPrato Principal: " << *aux2 << "\n\tCusto:" << r->custo << "€\n";
+	string *FIM = new string[7];
+	FIM[0] = "Refeição atual:\n\tEntrada: ";
+	FIM[1] = *aux1;
+	FIM[2] = "\n\tPrato Principal: ";
+	FIM[3] = *aux2;
+	FIM[4] = "\n\tCusto:";
+	FIM[5] = to_string(r->custo);
+	FIM[6] = "€\n";
+	delete aux1, aux2;
+	return FIM;
+}
+
+mesa* criaCantina() {//vai obter o tamanho da cantina e gerar tamanhos de mesas até a cantina estar preenchida, tamanhos guardados num vetor
+	short *TCANTINA = new short(rand() % 21 + 30);
+	short *t = new short();
+	short *aux = new short[25];
+	short *itera = new short(0);
+	while (*t < *TCANTINA) {
+		short *TMESA = new short(rand() % 4 + 2);
+		*t += *TMESA;
+		aux[(*itera)++] = *TMESA;
+		if (*TCANTINA - *t <= 1 && *TCANTINA != *t) {//quando isto acontece, nenhuma mesa poderá preencher o último espaço ou já ultrapassamos o tamanho da cantina, por isso faz-se um "reset", "apaga-se" a mesa que provocou isto e tenta-se outra vez
+			*t -= *TMESA;
+			(*itera)--;
+		}
+		delete TMESA;
+	}
+	short *vtamMesas = copiaVetor(aux, *itera);
+	mesa* vMes= criaMesas(vtamMesas, *itera);
+	delete itera, TCANTINA, t;
+	delete[]aux;
+	return vMes;
+}
+
+mesa* criaMesas(short *vtamMesas, short tam) {//vai criar as mesas da cantina com o vetor que contem os tamanhos das mesas obtidos em criaCantina
+	short *itera = new short(0);
+	mesa* vMesas = new mesa[tam];
+	short *num = new short(1);
+	while (*itera < tam) {
+		mesa *m = new mesa();
+		m->numMesa = *num;
+		(*num)++;
+		m->tamanho = vtamMesas[*itera];
+		pessoa *sit = new pessoa[vtamMesas[*itera]];
+		m->sentados = sit;
+		guardaVetorMesas(m,vMesas, *itera);
+		(*itera)++;
+	}
+	return vMesas;
+}
+
+void guardaVetorMesas(mesa* m, mesa* vetor, short pos) {
+	vetor[pos] = *m;
+}
+
+void escreveMesa(mesa *m) {
+	cout << "Mesa " << m->numMesa << "(CAPACIDADE " << m->tamanho << "):\n";
+}
+
+void preencheMesa(mesa *m, pessoa *fila) {
+	short*i = new short(0);
+	short*j = new short(0);
+	while (*i < m->tamanho) {
+		m->sentados[(*i)++] = fila[(*j)++];
+	}
+
+}
+void testeMesas(mesa *m,pessoa *p) { //verifica as mesas pra ver se tem cursos diferentes
+	short i = 0;
+	bool funciona = false;
+	while (i < m->tamanho) {
+		if (p->membro_aluno.num > 0) {
+			if (m->sentados[i].membro_aluno.curso == p->membro_aluno.curso) {
+				funciona = true;
+			}
+		}
+		cout << m->sentados[i].membro_aluno.curso << p->membro_aluno.curso << endl;
+		i++;
+	}
+	cout << funciona << endl;
+}
--- Pessoas.cpp
+++ Pessoas.cpp
@@ -1,127 +1,133 @@
 #include"Mesas.h"
 #include"Pessoas.h"
+#include"Ficheiros.h"
 #include<iostream>
 #include<string>
 #include<fstream>
-#include "Ficheiros.h"
 using namespace std;
 
-
-void opcoes() {
-
-	char opcao;
-	bool sair = false;
-
-	while (!sair) {
-		cout << "\nEscolha uma opcao:\n";
-		cout << " (s) Seguinte \n";
-		cout << " (e) Emergência \n";
-		cout << " (g) Gravar \n";
-		cout << " (c) Carregar dados \n";
-		cout << " (o) Opções \n";
-		cin >> opcao;
-		cout << endl;
-		switch (opcao) {
-		case ('s'):
-			cout << " Escolheu Seguinte \n";
-			/*linha para fazer a subtraçao do ciclo do tempo de cada refeicao na cantina*/
-			break;
-
-		case ('e'):
-			char opcao1;/*opcao dentro da emergencia para escolher entre grupo ou aluno a abandonar*/
-			cout << " Escolheu Emergência \n";
-			cout << " Escolha a opção:\n1 - Remover Aluno/Funcionário\n2 - Remover Grupo/departamento \n";
-			cin >> opcao1;
-
-			switch (opcao1) {
-			case 1:
-				cout << " Insira o número de identificação do aluno/funcionário \n";
-				/*falta o cin do numero de ident da pessoa*/
-				/*linha para cobrar a refeicao da pessoa*/
-				/*linha para remover a pessoa da cantina*/
-				/*linha para inserir novos indivíduos na cantina para preencher*/
-				break;
-
-			case 2:
-			
-				cout << " Insira o número de identificação do grupo \n";
-				/*falta o cin do numero de ident do grpo*/
-				/*linha para cobrar refeicao*/
-				/*linha para reover grpo da cantina*/
-				/*linha para inserir novo grupo na cantina para preencher*/
-				break;
-			}
-
-		case ('g'):
-			cout << " Escolheu Gravar \n";
-			escreveFicheiro;
-			break;
-
-		case ('c'):
-			cout << " Escolheu Carregar Dados \n";
-			break;
-
-		case ('o'):
-			int opcao2 = 0;
-			cout << " Escolheu Opções \n";
-			cout << " Escolha a opção: \n1 - Mostrar todos os indivíduos do sistema\n2 - Mostrar todas as mesas\n3 - Mostrar todos os indivíduos rejeitados por falta de plafond\n4 - Alterar o plafond do indivíduo enquanto este está na fila\n5 - Apresentação dos indivíduos de um determinado departamento\n6 - Editar a duração da refeição de um grupo/departamento\n7 - Pesquisa de indivíduos com base no seu id\n8 - Editar nome de um indivíduo\n ";
-			cin >> opcao2;
-
-			switch (opcao2) {
-			case 1:
-				/*mostrar todos os individuos do sistema*/
-				break;
-
-			case 2:
-				/*mostrar todas as mesas*/
-				break;
-
-			case 3:
-				/*mostrar todos os individos rejeitados por falta de plafond*/
-				break;
-
-			case 4:
-				/*alterar o plafond do individuo em espera na fila*/
-				break;
-
-			case 5:
-				/*apresentaçao dos individuos de determinado curso*/
-				break;
-
-			case 6:
-				/*editar a duração de uma refeição de um grupo/departamento*/
-				break;
-
-			case 7:
-				/*pesquisa com base no numero de aluno*/
-				break;
-
-			case 8:
-				/*editar o nome de um individuo*/
-				break;
-
-			}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-		}
-
+pessoa* criaPessoa(string *pnomes, string *unomes, string curso, short dura, bool alunoOuNao, bool especialOuNao) {
+	pessoa *p = new pessoa();
+	int *NUMALUNO = new int((20 * 100000) + ((rand() % 999 + 1) * 100) + (rand() % 7 + 12));
+	int *NUMSTAFF = new int((19 * 100000) + ((rand() % 999 + 1) * 100) + (rand() % 31 + 70));
+	short *RETIRAPNOME = new short(rand() % 44);
+	short *RETIRAUNOME = new short(rand() % 97);
+	float *PLAF = new float(rand() % 100 + 1);
+	p->priNome = pnomes[*RETIRAPNOME];
+	p->ultNome = unomes[*RETIRAUNOME];
+	p->plafond = *PLAF;
+	p->duração = dura;
+	if (alunoOuNao) {
+		p->membro_aluno.curso = curso;
+		p->membro_aluno.num = *NUMALUNO;
+		p->membro_aluno.especialOuNao = especialOuNao;
 	}
-}	
+	else {
+		p->membro_staff.numFuncionario = *NUMSTAFF;
+	}
+	delete RETIRAPNOME, RETIRAUNOME, PLAF, NUMSTAFF;
+	return p;
+}
+
+pessoa* criaGrupo(string *pnomes, string *unomes, string*cursos) {
+	short *RETIRACURSO = new short(rand() % 19);
+	short *DURAÇÃOMEAL = new short(rand() % 4 + 2);
+	bool *alea = new bool(rand() % 2);
+	short *PROBESP = new short(rand() % 100 + 1);
+	bool *esp = new bool();
+	if (*PROBESP > 15 && *PROBESP <= 20) {
+		*esp = true;
+	}
+	else {
+		*esp = false;
+	}
+	if (*esp) {
+		pessoa *k = new pessoa[1];
+		pessoa *p = criaPessoa(pnomes, unomes, cursos[*RETIRACURSO], *DURAÇÃOMEAL, true, *esp);
+		short *GRUPOIDDEPART = new short(rand() % 401 + 100);/*
+		if (!testaIDDepartamento(*GRUPOIDDEPART, res, iterato)) {
+			*GRUPOIDDEPART =(rand() % 401 + 100);
+		}*/
+		p->numDepartamentoOuGrupo = *GRUPOIDDEPART;
+		p->tamanho = 1;
+		k[0] = *p;
+		delete p;
+		return k;
+	}
+	else {
+		short *TAMANHO = new short(rand() % 10 + 1);
+		pessoa *l = new pessoa[*TAMANHO];
+		short *itera = new short(0);
+		short *GRUPOIDDEPART = new short(rand() % 401 + 100);/*
+		if (!testaIDDepartamento(*GRUPOIDDEPART, res, iterato)) {
+			*GRUPOIDDEPART = (rand() % 401 + 100);
+		}*/
+		for (*itera; *itera < *TAMANHO; (*itera)++) {
+			pessoa*p = criaPessoa(pnomes, unomes, cursos[*RETIRACURSO], *DURAÇÃOMEAL, *alea, *esp);
+			p->numDepartamentoOuGrupo = *GRUPOIDDEPART;
+			if (*itera == 0) {
+				p->tamanho = *TAMANHO;
+			}
+			l[*itera] = *p;
+			delete p;
+		}
+		delete TAMANHO, itera;
+		return l;
+	}
+	delete RETIRACURSO, esp;
+}
+
+void escrevePessoa(pessoa *p) {
+	if (p->membro_aluno.num > 0) {
+		if (!p->membro_aluno.especialOuNao) {
+			cout << p->priNome << " " << p->ultNome << ", Estudante, Grupo " << p->numDepartamentoOuGrupo << ", " << p->membro_aluno.curso << " , duração " << p->duração << ", " << p->plafond << "€\n";
+		}
+		else {
+			cout << p->priNome << " " << p->ultNome << ", Estudante (especial), Grupo " << p->numDepartamentoOuGrupo << ", " << p->membro_aluno.curso << " , duração " << p->duração << ", " << p->plafond << "€\n";
+		}
+	}
+	else {
+		cout << p->priNome << " " << p->ultNome << ", Staff, Departamento " << p->numDepartamentoOuGrupo << ", " << p->membro_staff.numFuncionario << ", duração " << p->duração << ", " << p->plafond << "€\n";
+	}
+}
+pessoa* preencheFila(pessoa*fila, string* pnomes, string*unomes, string*cursos,short*i) {
+	short*j = new short(0);
+	pessoa*g = criaGrupo(pnomes, unomes, cursos);
+	short*tam = new short(g[0].tamanho);
+	while(*i<50) {
+		for(*j=0;*j<*tam;(*j)++){
+			fila[*i] = g[(*j)];
+		}
+		g = criaGrupo(pnomes, unomes, cursos);
+		*tam = g[0].tamanho;
+		(*i)++;
+	}
+	delete i, j;
+	return fila;
+}
+
+void escreveFila(pessoa*fila) {
+	short*i = new short(0);
+	while (*i < 50) {
+		escrevePessoa(&fila[(*i)++]);
+	}
+}
+
+void alterarPlafond(int n, pessoa*fila) {
+	short i = 0;
+	if (fila->membro_aluno.num > 0) {
+		cout << "Plafond inicial: "<< fila->plafond << "€" <<endl;
+		cout << "Insira um novo plafond: " ;
+		cin >> fila->plafond; 
+		cout << "Plafond atual: " << fila->plafond<< "€" << endl;
+		cout << fila->membro_aluno.num;
+	}
+	else {
+		cout << "Plafond inicial: " << fila->plafond << "€" << endl;
+		cout << "Insira um novo plafond: ";
+		cin >> fila->plafond;
+		cout << "Plafond atual: " << fila->plafond << "€" << endl;	
+		cout << fila->membro_staff.numFuncionario;
+	}
+}
+
